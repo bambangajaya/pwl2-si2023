a9a7373f8d15e0b8e0aa672af03d674a5ceabaaf
bambangsangdewa <bambangaja2004@gmail.com>
2024-09-11 17:47:20 +0700

    Module Product

---
+++ resources/views/products/index.blade.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data products</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body style="background: lightgray">
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-12">
+                <div>
+                    <h3 class="text-center my-4">Tutorial Laravel 11</h3>
+                    <hr>
+                </div>
+                <div class="card border-0 shadow-sm rounded">
+                    <div class="card-body">
+                        <a href="{{ route('products.create') }}" class="btn btn-md btn-success mb-3">Add Product</a>
+                        <table class="table table-bordered">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Image</th>
+                                    <th scope="col">Title</th>
+                                    <th scope="col">Category</th>
+                                    <th scope="col">Price</th>
+                                    <th scope="col">Stock</th>
+                                    <th scope="col" style="width: 20%">Actions</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @forelse ($products as $product)
+                                    <tr>
+                                        <td scope="text-center">
+                                            <img src="{{ asset('/storage/products/'.$product->image) }}" class="rounded" style="width: 150px">
+                                        </td>
+                                        <td>{{ $product->title }}</td>
+                                        <td>{{ $product->product_category_name }}</td>
+                                        <td>{{ "Rp " . number_format($product->price,2,',','-') }}</td>
+                                        <td>{{ $product->stock }}</td>
+                                        <td class="text-center">
+                                            <form onsubmit="return confirm('Apakah Anda Yakin ?');" action="{{ route('products.destroy', $product->id) }}" method="POST">
+                                                <a href="{{ route('products.show', $product->id) }}" class="btn btn-sm btn-dark">Show</a>
+                                                <a href="{{ route('products.edit', $product->id) }}" class="btn btn-sm btn-primary">Edit</a>
+                                                @csrf
+                                                @method('DELETE')
+                                                <button type="submit" class="btn btn-sm btn-danger">Hapus</button>
+                                            </form>
+                                        </td>
+                                    </tr>
+                                @empty
+                                <div class="alert alert-danger">
+                                    Data Products belum Tersedia.
+                                </div>
+                                @endforelse
+                            </tbody>
+                        </table>
+                        {{ $products->links() }}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+    <script>
+        @if(session('success'))
+            Swal.fire({
+                icon: "success",
+                title: "Berhasil",
+                text: "{{ session('success') }}",
+                showConfirmButton: false,
+                timer: 2000
+            });
+        @elseif(session('error'))
+            Swal.fire({
+                icon: "error",
+                title: "Gagal",
+                text: "{{ session('error') }}",
+                showConfirmButton: false,
+                timer: 2000
+            });
+        @endif
+    </script>
+</body>
+</html>
